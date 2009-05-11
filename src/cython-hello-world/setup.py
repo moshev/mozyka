5d674279995b843120c303f8b39ff04ae7b8a9c2
Dimitar Toshev (Димитър Тошев) <mo6eeeb@gmail.com>
2009-05-11 14:33:11 +0300

    Скрипт за конструиране на питонски разширения

--- src/cython-hello-world/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-
-def get_extensions():
-    return [Extension("helloworld", ["helloworld.pyx"]),]
-
