b804193581b11887090d9f4cc1003e949f6609cc
Dimitar Toshev <mo6eeeb@gmail.com>
2009-07-19 13:20:33 +0300

    Split tests

--- src/render_test.py
+++ src/render_test.py
@@ -1,14 +1,5 @@
 import plum
 import geometry
-import time
-
-class Dragger:
-    def start(self, x, y):
-        print("Starting drag at {0}, {1}".format(x, y))
-    def drag(self, x, y, xrel, yrel):
-        print("Now at ({0}, {1}), vector ({2}, {3})".format(x, y, xrel, yrel))
-    def stop(self, x, y):
-        print("Stopping drag at {0}, {1}".format(x, y))
 
 class Cube(geometry.Model):
     vertices = [
@@ -58,11 +49,9 @@ gl = plum.GL(800, 600)
 cube = Cube()
 render = Renderer(cube, gl)
 
-w.key.add_handler(plum.keycode['a'], lambda state: print("Pressed A") if state == plum.keystate.PRESSED else print("Boo"))
 w.key.add_handler(plum.keycode['b'], lambda state: render() if state == plum.keystate.RELEASED else False)
-w.mouse.add_clickhandler(1, lambda x, y: print("LMB clicked at {0}, {1}.".format(x, y)))
-w.mouse.add_draghandler(1, Dragger())
 
+render()
 w.main_loop()
 
 plum.quit()
